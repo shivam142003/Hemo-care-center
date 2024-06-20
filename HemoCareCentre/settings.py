e42f161031c271f2ca2a37c145c498f332b7c411
Shivam <121782205+shivam142003@users.noreply.github.com>
2024-06-20 16:14:27 +0530

    Update settings.py

--- HemoCareCentre/settings.py
+++ HemoCareCentre/settings.py
@@ -118,9 +118,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS=[
-STATIC_DIR,
- ]
+STATICFILES_DIRS= os.path.join(BASE_DIR,'static'),
+STATIC_ROOT= os.path.join(BASE_DIR,'staticfiles_build','static')
 LOGIN_REDIRECT_URL='/afterlogin'
 
 #for contact us give your gmail id and password
