07cbc220e43d20aae42c7b92d3b9ad7cb57a6d70
Shivam <121782205+shivam142003@users.noreply.github.com>
2024-06-20 16:17:17 +0530

    Update urls.py

--- HemoCareCentre/urls.py
+++ HemoCareCentre/urls.py
@@ -35,3 +35,6 @@ urlpatterns = [
     path('update-reject-status/<int:pk>', views.update_reject_status_view,name='update-reject-status'),
    
 ]
+
+urlpatterns+= static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+urlpatterns+= static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
